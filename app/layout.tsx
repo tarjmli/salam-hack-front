0ae85aafa321bd2da95bcaf1025d169e27a9406e
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-03-20 15:23:32 +0100

    Add suppressHydrationWarning to html tag to prevent hydration warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" dir="rtl">
+    <html lang="en" dir="rtl" suppressHydrationWarning>
       <body className={`${spaceGrotesk.className} bg-black text-white pb-20`}>
         <Toaster />
         <ReactQueryProvider>
